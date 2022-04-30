ae4a2028b2b1a063d48a27a3448e14a409f0821b
angelogstec <angelogstec@gmail.com>
2022-04-30 18:57:31 -0300

    Modified the command manager and removed unused imports

--- src/main/java/me/agns/snowevent/commands/TeamCommand.java
+++ src/main/java/me/agns/snowevent/commands/TeamCommand.java
@@ -1,21 +1,24 @@
 package me.agns.snowevent.commands;
 
-import me.agns.snowevent.SnowEvent;
-import me.agns.snowevent.Team;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
 
 public class TeamCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Use /team <add, create, remove, delete>");
+        if (args[0].equalsIgnoreCase("add")){
+            new AddCommand().onCommand(sender, command, label, args);
+        }else if (args[0].equalsIgnoreCase("create")){
+            new CreateCommand().onCommand(sender, command, label, args);
+        }else if(args[0].equalsIgnoreCase("delete")){
+            new DeleteCommand().onCommand(sender, command, label, args);
+        }else if (args[0].equalsIgnoreCase("remove")){
+            new RemoveCommand().onCommand(sender, command, label, args);
+        }else {
+            sender.sendMessage("Use /team <add, create, remove, delete>");
+        }
         return true;
     }
 }
